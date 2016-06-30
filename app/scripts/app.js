daef96e182575bc7a70d3ae8ec486114d4a59a08
Maicon Gomes <dev.mrgomes@outlook.com>
2016-06-29 22:57:05 -0300

    push project

---
+++ app/scripts/app.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name scheduleApp
+ * @description
+ * # scheduleApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('scheduleApp', [
+    'ngResource',
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
